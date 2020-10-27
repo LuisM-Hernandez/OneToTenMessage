25c558e1bed51debec8e1fc51afdf960416ff418
Luis <m.luishernandez@outlook.com>
2020-10-27 03:15:13 -0400

    finished the program

--- Program.cs
+++ Program.cs
@@ -6,7 +6,21 @@ namespace OneToTenMessage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            while(true) 
+            {
+                Console.WriteLine("Choose a number between 1-10");
+                int num = Int32.Parse(Console.ReadLine());
+
+                while(num<=10)
+                {
+                     for (int message=0; num>message; num--)
+            {
+                Console.WriteLine("Great");
+            }
+                }
+            
+            }
         }
     }
 }
